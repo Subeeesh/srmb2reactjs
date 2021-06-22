c6254c5a99d2abc7998fa7e3fbb86558d2858907
Subeeesh <64542922+Subeeesh@users.noreply.github.com>
2021-06-22 23:07:05 +0530

    Create project5.js

---
+++ project5.js
@@ -0,0 +1,15 @@
+//program for determine odd and even by adding prefix as dr and suffix as er.
+
+const username = ['subeesh','mahesh','guna'];
+const alter = username.map((uname, idx) => {
+    if (idx % 2 == 0) {
+        return 'Er.' + uname;
+    }
+    else {
+        return 'Dr.' + uname;
+    }
+}
+);
+for (let i = 0; i < alter.length; i++) {
+    console.log(alter[i]);
+}
